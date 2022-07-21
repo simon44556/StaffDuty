9fa2ad2f8c8e943d413e40f5fd9fd490c263682c
Simon Urbanija <simon@u-centrix.com>
2022-07-21 19:16:28 +0200

    - Redo luckperms events

--- src/main/java/me/Midnight/StaffDuty/EventManager/LuckpermsEvents.java
+++ src/main/java/me/Midnight/StaffDuty/EventManager/LuckpermsEvents.java
@@ -35,17 +35,7 @@ public class LuckpermsEvents {
         Collection<? extends Player> onlinePlayers = Bukkit.getOnlinePlayers();
 
         for (Player p : onlinePlayers) {
-            User user = luckPerms.getUserManager().getUser(p.getUniqueId());
-
-            if (user == null) {
-                continue;
-            }
-
-            if (p.hasPermission("toggleduty.staff") || p.isOp()) {
-                trackManager.updateOrAddPrefixes(Bukkit.getPlayer(user.getUniqueId()));
-            } else {
-                trackManager.updatePrefixes(Bukkit.getPlayer(user.getUniqueId()));
-            }
+            trackManager.updateOrAddPrefixes(p);
         }
     }
 
@@ -61,14 +51,6 @@ public class LuckpermsEvents {
             return;
         }
 
-        if (player.isOnline()
-                && user.getCachedData().getPermissionData().checkPermission("toggleduty.staff").asBoolean()
-                || player.isOp()) {
-            trackManager.updateOrAddPrefixes(player);
-        }
-
-        else if (player.isOnline()) {
-            trackManager.updatePrefixes(player);
-        }
+        trackManager.updateOrAddPrefixes(player);
     }
 }
